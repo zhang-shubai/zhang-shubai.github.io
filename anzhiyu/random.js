91e3b9635ef5a656ba266b752b6e8d05a51d1a2c
zhang-shubai <huangming.zzz1368@gmail.com>
2025-07-17 14:36:26 +0800

    Site updated: 2025-07-17 14:36:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/15/hello-world/","2025/07/17/Jailbreaking-is-Mostly-Simpler-Than-You-Think/","2025/07/15/hexo基本操作/"];function toRandomPost(){
+var posts=["2025/07/17/Jailbreaking-is-Mostly-Simpler-Than-You-Think/","2025/07/15/hello-world/","2025/07/15/hexo基本操作/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
